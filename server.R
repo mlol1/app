be7616f3bd26af6589707d486721f8af98decb7e
mlol1 <94497311@umail.ucc.ie>
2014-07-06 11:21:39 +0100

    Updates

--- server.R
+++ server.R
@@ -1,30 +1,114 @@
-
+#based on code from https://gist.github.com/dgrapov/5792778 and shiny tutorials http://shiny.rstudio.com/tutorial/
 library(sp)
 library(RColorBrewer)
 
 counties <- read.csv("http://cs1.ucc.ie/~mlol1/CS6500/data/Percentage_Completeness.csv")
 counties<-as.data.frame(counties)
-
+Counties_numbers<-read.csv("http://cs1.ucc.ie/~mlol1/CS6500/Counties_numbers.csv")
+Counties_numbers$x<-NULL
 source("helpers.R")
-shinyServer(
-  function(input, output) {
-    output$spplot <- renderPlot({
-
+# shiny server side code for each call
+shinyServer(function(input, output, session){
+  output$spplot <- renderPlot({
+    
     args <- switch(input$var, 
- "Percent Complete: ALL NACE CODES" = list((counties$NACECODE_ALL_PERCENT)*100 ,"darkgreen","% Complete: ALL NACE CODES"),
- "Percent Complete: G WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES" = list((counties$NACECODE_G_PERCENT)*100,"black"," % Complete: G"),
- "Percent Complete: S OTHER SERVICE ACTIVITIES" = list((counties$NACECODE_S_PERCENT)*100,"darkorange","Complete: S"),
- "Percent Complete: O PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY" = list((counties$NACECODE_O_PERCENT)*100,"darkblue","% Complete: O"),
- "Percent Complete: I ACCOMMODATION AND FOOD SERVICE ACTIVITIES" = list((counties$NACECODE_I_PERCENT)*100,"red","% Complete: I"),
- "Percent Complete: Q HUMAN HEALTH AND SOCIAL WORK ACTIVITIES" = list((counties$NACECODE_Q_PERCENT)*100,"brown","% Complete: Q"),
- "Percent Complete: P Education" = list((counties$NACECODE_P_PERCENT)*100,"violet","% Complete: P"))
+                   "Percent Complete: ALL NACE CODES" = list((counties$NACECODE_ALL_PERCENT)*100 ,"darkgreen","% Complete: ALL NACE CODES"),
+                   "Percent Complete: G WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES" = list((counties$NACECODE_G_PERCENT)*100,"black"," % Complete: G"),
+                   "Percent Complete: S OTHER SERVICE ACTIVITIES" = list((counties$NACECODE_S_PERCENT)*100,"darkorange","Complete: S"),
+                   "Percent Complete: O PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY" = list((counties$NACECODE_O_PERCENT)*100,"darkblue","% Complete: O"),
+                   "Percent Complete: I ACCOMMODATION AND FOOD SERVICE ACTIVITIES" = list((counties$NACECODE_I_PERCENT)*100,"red","% Complete: I"),
+                   "Percent Complete: Q HUMAN HEALTH AND SOCIAL WORK ACTIVITIES" = list((counties$NACECODE_Q_PERCENT)*100,"brown","% Complete: Q"),
+                   "Percent Complete: P Education" = list((counties$NACECODE_P_PERCENT)*100,"violet","% Complete: P"))
+    
+    args$min <- input$range[1]
+    args$max <- input$range[2]
+    
+    do.call(percent_spplot, args)
+  })
+  
+  #update variable and group based on dataset
+  output$variable <- renderUI({ 
+    obj<-switch(input$dataset,
+                "Counties_numbers" = Counties_numbers)   
+    var.opts<-namel(colnames(obj))
+    selectInput("variable","Variable:", var.opts) # uddate UI 				 
+  }) 
+  
+  output$group <- renderUI({ 
+    obj<-switch(input$dataset,
+                "Counties_numbers" = Counties_numbers)	 
+    var.opts<-namel(colnames(obj))
+    selectInput("group","Groups:", var.opts) # uddate UI 				 
+  }) 
+  
+  output$caption<-renderText({
+    switch(input$plot.type,
+           "plot" 	= 	"plot",
+           "histogram" =	"Histogram",
+           "density" 	=	"Density plot",
+           "bar" 		=	"Bar graph")
+  })
+  
+  
+  output$plot <- renderUI({
+    plotOutput("p")
+  })
+  
+  #plotting function using ggplot2
+  output$p <- renderPlot({
+    
+    plot.obj<<-list() # not sure why input$X can not be used directly?
+    plot.obj$data<<-get(input$dataset) 
+    plot.obj$variable<<-with(plot.obj$data,get(input$variable)) 
+    plot.obj$group<<-with(plot.obj$data,get(input$group)) 
+    
+    #dynamic plotting options
+    plot.type<-switch(input$plot.type,
+                      "boxplot" 	= 	geom_boxplot(),
+                      "histogram" =	geom_histogram(alpha=0.5,position="identity"),
+                      "density" 	=	geom_density(alpha=.75),
+                      "bar" 		=	geom_bar(position="dodge")
+    )
+    
+    require(ggplot2)
+    #plotting theme
+    .theme<- theme(
+      axis.line = element_line(colour = 'darkgray', size = .75), 
+      panel.background = element_blank(),  
+      plot.background = element_blank()
+    )	 
+    if(input$plot.type=="boxplot")	{		#control for 1D or 2D graphs 
+      p<-ggplot(plot.obj$data, 
+                aes(
+                  x 		= plot.obj$group, 
+                  y 		= plot.obj$variable,
+                  fill 	= as.factor(plot.obj$group)
+                )
+      ) + plot.type
       
-args$min <- input$range[1]
-args$max <- input$range[2]
-
-do.call(percent_spplot, args)
-    })
-
-
-  }
-)
+      if(input$show.points==TRUE)
+      { 
+        p<-p+ geom_point(color='black',alpha=0.5, position = 'jitter')
+      }
+      
+    } else {
+      
+      p<-ggplot(plot.obj$data, 
+                aes(
+                  x 		= plot.obj$variable,
+                  fill 	= as.factor(plot.obj$group),
+                  group 	= as.factor(plot.obj$group)
+                 # color 	= as.factor(plot.obj$group)
+                )
+      ) + plot.type
+    }
+    
+    p<-p+labs(
+      fill 	= input$group,
+      x 		= "",
+      y 		= input$variable
+    )  +
+      .theme
+    print(p)
+  })	
+})
--- ui.R
+++ ui.R
@@ -1,34 +1,46 @@
+## UI for app
 shinyUI(fluidPage(
-  titlePanel("Percentage Completeness Visualisations for identifiable non-residential buildings in OSM V GEODIRECTORY"),
+  titlePanel("Visualisations for identifiable non-residential buildings in OSM V GEODIRECTORY"),
   
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Choose from the selection of Nace Codes to see the different completeness rates"),
-      
-      selectInput("var", 
-                  label = "Choose a variable to display",
-                  choices = c("Percent Complete: ALL NACE CODES", "Percent Complete: G WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES",
-                              "Percent Complete: S OTHER SERVICE ACTIVITIES" ,
-                              "Percent Complete: O PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY" ,
-                              "Percent Complete: I ACCOMMODATION AND FOOD SERVICE ACTIVITIES",
-                              "Percent Complete: Q HUMAN HEALTH AND SOCIAL WORK ACTIVITIES" ,
-                              "Percent Complete: P Education"
-                      ),
-                  selected = "Percent Complete: ALL NACE CODES"),
+#shinyUI(pageWithSidebar(
+  # title
+  headerPanel("Select Options"),
+  
+  #input
+  sidebarPanel
+  (
+    helpText("Choose from the selection of Nace Codes to see the different completeness rates"),
+    selectInput("var", 
+                label = "Choose a variable to display",
+                choices = c("Percent Complete: ALL NACE CODES", "Percent Complete: G WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES",
+                            "Percent Complete: S OTHER SERVICE ACTIVITIES" ,
+                            "Percent Complete: O PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY" ,
+                            "Percent Complete: I ACCOMMODATION AND FOOD SERVICE ACTIVITIES",
+                            "Percent Complete: Q HUMAN HEALTH AND SOCIAL WORK ACTIVITIES" ,
+                            "Percent Complete: P Education"
+                ),
+                selected = "Percent Complete: ALL NACE CODES"),
     
-      
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
+    sliderInput("range", 
+                label = "Range of interest:",
+                min = 0, max = 100, value = c(0, 100)),
+    
+    helpText("Choose between box plot, histogram, density plot or bar plot "),
+    selectInput("dataset","Data:", 
+                list(Counties_numbers = "Counties_numbers")),
+    uiOutput("variable"), 	# depends on dataset ( set by output$variable in  server.R)
+    uiOutput("group"),  		# depends on dataset	( set by output$group in  server.R)
+    selectInput("plot.type","Plot Type:", 
+                list(boxplot = "boxplot", histogram = "histogram", density = "density", bar = "bar")
     ),
-    
-
+    checkboxInput("show.points", "show points", TRUE)
+  ),	
   
-    mainPanel(
-      plotOutput("spplot")
-    )
-    
-    )
-    
+  # output				
+  mainPanel(
+    plotOutput("spplot"),
+    h3(textOutput("caption")),
+    #h3(htmlOutput("caption")),
+    uiOutput("plot") # depends on input 
   )
-)
+))
