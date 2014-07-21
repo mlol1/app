f3859fb2bd5fc0b833afb858068815c299abbbe3
mlol1 <mlol1@users.noreply.github.com>
2014-07-21 15:45:20 +0100

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -20,6 +20,7 @@ percent_spplot <- function(var, color, legend.title, min = 0, max = 100) {
 #
 plot.new()
 
+
 # plot choropleth map
 
  print(spplot(gadm,"NAME_1",colorkey=TRUE, col.regions = fills, add = TRUE,lty = 0))
@@ -29,22 +30,16 @@ plot.new()
 print(spplot(gadm,"NAME_1", col = "black", col.regions = fills, add = TRUE, lty = 1, lwd = 1))
   
   # add a legend
-inc <- (max - min) / 10
+inc <- (max - min) / 4
 legend.text <- c(paste0(min, "%"),
                  paste0(min + inc, "%"),
                  paste0(min + 2 * inc, "%"),
                  paste0(min + 3 * inc, "%"),
-                 paste0(min + 4 * inc, "%"),
-                 paste0(min + 5 * inc, "%"),
-                 paste0(min + 6 * inc, "%"),
-                 paste0(min + 7 * inc, "%"),
-                 paste0(min + 8 * inc, "%"),
-                 paste0(min + 9 * inc, "%"),
                  paste0(max, "%"))
 
 legend("bottomleft", 
        legend = legend.text, 
-       fill = shades[c(0, 10, 20, 30, 40,50,60,70,80,90,100)], 
+       fill = shades[c(25,50,75)], 
        title = legend.title)
 
 
