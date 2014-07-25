8ec1894d2587e3fea34fbdefb0d310b02e24aaf8
mlol1 <mlol1@users.noreply.github.com>
2014-07-25 18:51:53 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,74 +3,80 @@ shinyUI(fluidPage(
   titlePanel("Visualisations for identifiable non-residential buildings in OSM V GEODIRECTORY"),
   
   #input
-  sidebarPanel
-  (
-    helpText("Choose from the selection of Nace Codes to see the different completeness rates"),
-    selectInput("var", 
-                label = "Choose a variable to display",
-                choices = c("Percent Complete: ALL NACE CODES", "Percent Complete: G WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES",
-                            "Percent Complete: S OTHER SERVICE ACTIVITIES" ,
-                            "Percent Complete: O PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY" ,
-                            "Percent Complete: I ACCOMMODATION AND FOOD SERVICE ACTIVITIES",
-                            "Percent Complete: Q HUMAN HEALTH AND SOCIAL WORK ACTIVITIES" ,
-                            "Percent Complete: P Education"
-                ),
-                selected = "Percent Complete: ALL NACE CODES"),
-    
-    sliderInput("range", 
-                label = "Range of interest:",
-                min = 0, max = 100, value = c(0, 100),animate=TRUE),
-    fluidRow(
-      column(4, 
-             selectInput("char1", "",
-                         choices =levels(counties_rows$variable)))),
-    
-    helpText("Display line charts, box plot, scatter plot, bubble charts, histogram, density plot for the compared variables "),    
-    selectInput("OSM",label = "Choose an OpenStreetMap (OSM) variable to compare",
-                choices= c(
-                  "OSM_ALL",                  
-                  "OSM_S",
-                  "OSM_G",                  
-                  "OSM_O",                  
-                  "OSM_P",                  
-                  "OSM_I",                  
-                  "OSM_Q"
-                ),selected = "OSM_ALL"),
-    
-    selectInput("GEODIR",label = "Choose a Geodirectory (GEODIR) variable to compare",
-                choices= c(
-                  "GEODIR_ALL",
-                  "GEODIR_S",
-                  "GEODIR_G",
-                  "GEODIR_O",
-                  "GEDOIR_P",                  
-                  "GEODIR_I",                  
-                  "GEODIR_Q"),selected = "GEODIR_ALL"),
-    sliderInput("binsx",
-                "Number of bins:",
-                min = 0,
-                max = 50,
-                value = 30),
-    sliderInput("binsy",
-                "Number of bins:",
-                min = 100,
-                max = 200,
-                value = 100)
-  ),	
+sidebarPanel(
+    helpText(br("Choose from the selection of Nace Codes to see the different completeness rates"),
+             br("ALL NACE CODES"),   
+             br("G: WHOLESALE AND RETAIL TRADE;REPAIR OF MOTOR VEHICLES AND MOTORCYCLES"),
+             br("S: OTHER SERVICE ACTIVITIES"),
+             br("O: PUBLIC ADMINISTRATION AND DEFENCE;COMPULSORY SOCIAL SECURITY"),
+             br("I: ACCOMMODATION AND FOOD SERVICE ACTIVITIES"),
+             br("Q: HUMAN HEALTH AND SOCIAL WORK ACTIVITIES"),
+             br("P: Education"))
+
+  )	,
 
   # output				
-  mainPanel(
-    
+
+    ## <embed percentages heat map here 
+  mainPanel( 
+
     tabsetPanel(
-      tabPanel("Choropleth",h4("Choropleth Map of Percentage Completeness"),plotOutput("spplot"),  
+      tabPanel("Choropleth",h4("Choropleth Map of Percentage Completeness"),tags$embed(src="http://www.openheatmap.com/view.html?map=UnideadSaradaVestibulum", 
+                                                                            tags$style(".alignRight { align: left; }", media = "all", type = "text/css"), 
+                                                                            height = 850, width = 1000 ),
+              # plotOutput("spplot"),
+              helpText("Display line charts, box plot, scatter plot, bubble charts, histogram, density plot for the compared variables "),    
+              selectInput("OSM",label = "Choose an OpenStreetMap (OSM) variable to compare",
+                          choices= c(
+                            "OSM_ALL",                  
+                            "OSM_S",
+                            "OSM_G",                  
+                            "OSM_O",                  
+                            "OSM_P",                  
+                            "OSM_I",                  
+                            "OSM_Q"
+                          ),selected = "OSM_ALL"),
+              
+              selectInput("GEODIR",label = "Choose a Geodirectory (GEODIR) variable to compare",
+                          choices= c(
+                            "GEODIR_ALL",
+                            "GEODIR_S",
+                            "GEODIR_G",
+                            "GEODIR_O",
+                            "GEDOIR_P",                  
+                            "GEODIR_I",                  
+                            "GEODIR_Q"),selected = "GEODIR_ALL"),
                htmlOutput("linech"),
                htmlOutput("barch"),
                htmlOutput("scatterch"),
-               htmlOutput("bubblech"),
                htmlOutput("histch"),
-               plotOutput("distPlot")),
-               tabPanel("Markers",h4("Marker representation of Percentage Completeness"),htmlOutput("googleVismerged"))
+              #plotOutput("density"),
+              sliderInput("binsx",
+                          "Number of bins for OSM:",
+                          min = 0,
+                          max = 50,
+                          value = 30),
+              sliderInput("binsy",
+                          "Number of bins for GEODIR:",
+                          min = 100,
+                          max = 200,
+                          value = 100),
+               plotOutput("distPlot"), height = "900px", width = "1000px"),
+               tabPanel("Markers",h4("Marker representation of Percentage Completeness"), fluidRow(
+                 column(4, 
+                        selectInput("char1", "",
+                                    choices =levels(counties_rows$variable)))),              
+
+                 htmlOutput("googleVismerged"),
+                 htmlOutput("bubblech"), width = "1000px", height = "900px"),
+      tabPanel("OSM Users",h4("check building locations, names and types"), tags$embed(src="http://www.openheatmap.com/view.html?map=CavitaryPurpurigenousNeuchtel", 
+                                                                                       tags$style(".alignRight { align: left; }", media = "all", type = "text/css"), 
+                                                                                       height = 750, width = 1000 ),
+               helpText("Log on to Openstreetmap to make updates"),
+               a(href="http://www.openstreetmap.org/search?query=Republic%20of%20Ireland#map=7/53.138/-7.530", "Click Here!")
+               
+              
       )
     )
 
-  ))
+  )))
